9ed7d5f119ac450ad8268e64fbc8611ff5cc0992
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2023-07-06 17:46:08 +0900

    메타수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,22 +2,29 @@
 import Header from "@/components/layout/Header";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import "./globals.scss";
+import Head from "next/head";
 
-export const metadata = {
-  title: "Portfolio by Next",
-  description: "create portfolio by Next",
-};
+// export const metadata = {
+//   title: "Portfolio by Next",
+//   description: "create portfolio by Next",
+// };
 
 export default function RootLayout({ children }) {
   const lenis = useLenis(({ scroll }) => {});
   return (
-    <html lang="ko">
-      <ReactLenis root>
-        <body>
-          <Header />
-          {children}
-        </body>
-      </ReactLenis>
-    </html>
+    <>
+      <Head>
+        <title>Portfolio by Next</title>
+        <meta name="description" content="Create portfolio by Next" />
+      </Head>
+      <html lang="ko">
+        <ReactLenis root>
+          <body>
+            <Header />
+            {children}
+          </body>
+        </ReactLenis>
+      </html>
+    </>
   );
 }
